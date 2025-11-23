cc58de56b2d79961bb50be93aa7e56dbd60a19fb
Ayush Biswas <biswasayush332@gmail.com>
2025-11-23 21:55:18 +0530

    instance variable at class level commit

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class Calculator{
+    
+    static double num1, num2, res;
+    static char oper;
+    static Scanner in = new Scanner(System.in);
     public static void main(String[]args){
-        Scanner in = new Scanner(System.in);
-        double num1, num2, res;
-        char oper;
+        
         System.out.println("Enter 1st Number: ");
         num1 = in.nextDouble();
         System.out.println("Enter the Operator(+,-,*,/): ");
         oper = in.next().charAt(0);
         System.out.println("Enter 2nd Number: ");
         num2 = in.nextDouble();
+        in.close();
         switch (oper) {
             case '+':
                 res = num1+num2;
